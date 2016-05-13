3eeb34a8ab468a0639e33eec7ac6d0968d17ecd9
Jay Cheroske <jay@portal.org>
2016-05-12 22:05:14 -0600

    renamed package

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-  name: 'jcheroske:responsive-media-queries',
+  name: 'jcheroske:reactive-media-queries',
   version: '0.0.1',
   // Brief, one-line summary of the package.
   summary: 'Reactive media queries',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/jcheroske/meteor-responsive-media-queries.git',
+  git: 'https://github.com/jcheroske/meteor-reactive-media-queries.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
